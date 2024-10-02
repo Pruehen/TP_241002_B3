8c6d057aefb404fb9b2965916bed23898434f7f2
JihoKwon <imperiaiiist@naver.com>
2024-10-02 10:43:17 +0900

    레벨 배치, 액터 생성 연습

--- Source/TP_241002_B3/MyActor1.cpp
+++ Source/TP_241002_B3/MyActor1.cpp
@@ -9,6 +9,7 @@ AMyActor1::AMyActor1()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	pMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("My MeshComp"));
 }
 
 // Called when the game starts or when spawned
